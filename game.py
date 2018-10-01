8a2a4d5eba115ae207bf4657d94016ddccf30552
Jorge Klemm <jorge.klemm@hotmail.com>
2018-10-01 00:38:29 -0300

    Red rect respect screen boundaries

--- game.py
+++ game.py
@@ -4,6 +4,9 @@ from pygame.locals import *
 FULLSCREEN = 0
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+MAX_WIDTH = 800
+MAX_HEIGHT = 600
+RESOLUTION = (MAX_WIDTH, MAX_HEIGHT)
 
 
 def main():
@@ -11,17 +14,17 @@ def main():
     y = 50
     width = 40
     height = 60
-    vel = 5
+    speed = 5
 
     pygame.init()
 
-    bestdepth = pygame.display.mode_ok((800, 600), FULLSCREEN, 32)
-    screen = pygame.display.set_mode((800, 600), FULLSCREEN, bestdepth)
+    bestdepth = pygame.display.mode_ok(RESOLUTION, FULLSCREEN, 32)
+    screen = pygame.display.set_mode(RESOLUTION, FULLSCREEN, bestdepth)
     pygame.display.set_caption('Pygame')
 
     running = True
     while running:
-        pygame.time.delay(100)
+        pygame.time.delay(30)
 
         for event in pygame.event.get():
             press_close_window = event.type == pygame.QUIT
@@ -30,14 +33,14 @@ def main():
                 running = False
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            x -= vel
-        if keys[pygame.K_RIGHT]:
-            x += vel
-        if keys[pygame.K_UP]:
-            y -= vel
-        if keys[pygame.K_DOWN]:
-            y += vel
+        if keys[pygame.K_LEFT] and x > 0:
+            x -= speed
+        if keys[pygame.K_RIGHT] and (x + width) < MAX_WIDTH:
+            x += speed
+        if keys[pygame.K_UP] and y > 0:
+            y -= speed
+        if keys[pygame.K_DOWN] and (y + height) < MAX_HEIGHT:
+            y += speed
 
         screen.fill(BLACK)
         pygame.draw.rect(screen, RED, (x, y, width, height))
